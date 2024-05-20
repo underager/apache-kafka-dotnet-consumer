15c36ab8f749da3f36d8ed125a9c2d0ced6e29ad
Atul Nirbhavane <anirbhavane106@gmail.com>
2024-05-21 00:31:26 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "gid-consumer",
+    BootstrapServers = "localhost:9002"
+};
+
+using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+{
+    consumer.Subscribe("rider-update");
+
+    while(true)
+    {
+        var bookingDetails = consumer.Consume();
+        Console.WriteLine(bookingDetails.Message.Value);
+    }
+}
+// Console.WriteLine("Hello, World!");
